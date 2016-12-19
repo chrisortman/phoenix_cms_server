af1b44d906c043ef48a580d8cada4a45f09dec7f
Chris Ortman <chris-ortman@uiowa.edu>
2016-12-19 15:36:49 -0600

    WIP

--- web/controllers/users_controller.ex
+++ web/controllers/users_controller.ex
@@ -3,6 +3,10 @@ defmodule CmsServer.UsersController do
 
   def index(conn, _params) do
     
+    db_props = %{protocol: "http", hostname: "localhost", database: "cms_backend", port: 5984}
+    {:ok, %{"rows" => [%{"value" => count}]}} = 
+      Couchdb.Connector.View.fetch_all(db_props, "howManySurveys", "how-many-surveys")
+      |> Couchdb.Connector.AsMap.as_map
     render conn, "index.html"
   end
 end
